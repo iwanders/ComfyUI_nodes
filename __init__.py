c66027ac7e9ae31bf0e1814b6000b8810c775a7d
Ivor Wanders <ivor@iwanders.net>
2024-06-21 19:41:52 -0400

    Node to save strings to disk.

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .save_string import SaveString
+
+NODE_CLASS_MAPPINGS = {
+    "SaveString": SaveString
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "SaveString": "SaveString"
+}
+
---
+++ save_string.py
@@ -0,0 +1,35 @@
+import folder_paths
+import os
+
+class SaveString:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "filename_prefix": ("STRING", { "multiline": False, "default": "ComfyUI"}),
+                "filename_suffix": ("STRING", { "multiline": False, "default": ""}),
+                "string_field": ("STRING", {"multiline": True, "dynamicPrompts": True, "defaultInput": True}),
+                "extension": (["txt", "json"], {"default": "txt"})
+            },
+            "optional": {
+            },
+        }
+
+    OUTPUT_NODE = True
+    RETURN_TYPES = tuple()
+
+    FUNCTION = "execute"
+
+    CATEGORY = "iw"
+
+    def execute(self, string_field, filename_prefix, filename_suffix, extension):
+        full_output_folder, filename, counter, subfolder, filename_prefix = folder_paths.get_save_image_path(filename_prefix, self.output_dir)
+        file = f"{filename}_{counter:05}_{filename_suffix}.{extension}"
+        path = os.path.join(full_output_folder, file)
+        with open(path, "w") as f:
+            f.write(string_field)
+        return tuple()
