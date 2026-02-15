94121ffdf74986a1ac031077ec26e57de312bc9e
Ivor Wanders <ivor@iwanders.net>
2026-02-15 10:26:00 -0500

    Bit more work on the structure.

--- web/ollama_generate_string.js
+++ web/ollama_generate_string.js
@@ -2,6 +2,10 @@ import { app } from "../../scripts/app.js";
 import { api } from "../../scripts/api.js";
 import { ComfyWidgets } from "../../scripts/widgets.js";
 
+
+// This is helpful:
+// https://github.com/Comfy-Org/ComfyUI_frontend/tree/0f0029ca29f4a11b37b84cd4d575280484bdd9e9/src/extensions/core
+
 app.registerExtension({
   name: "iw.ollama.generate",
 
@@ -64,10 +68,16 @@ app.registerExtension({
 
       nodeType.prototype.onExecuted = function (ui) {
           // This is after the fact... so we ready the next value.
-          console.log("onExecuted  next_value", ui.next_value);
-          let w = find_widget(this, "prompt");
-          w.value = ui.next_value;
-          w.setValue(w.value, false, true);
+          console.log("onExecuted  ui:", ui);
+          for (let orig_key of ["next_seed", "next_result", "next_prompt"]) {
+            let next_key = orig_key.replace("next", "use"); 
+            let w = find_widget(this, next_key);
+            if (w !== undefined) {
+              w.value = ui[orig_key][0];
+              w.setValue(w.value, false, true); 
+            }
+
+          }
 
       }
     }
